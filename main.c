ba4706c7fbd535c87429ff8c7f97eadf3dbd052d
Tanner Swanson <tannerswanson@protonmail.com>
2023-12-29 21:35:35 -0500

    Real vs predicted array sizes are incorrect. Once solved, reading / processing / printing data will be completed.

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 typedef struct {
     // Utilized by newLine and validate functions.
     int size;
+    int wordCount;
     char* array;
 } dynamic_array;
 
@@ -14,15 +15,16 @@ typedef struct {
 } signals;
 
 int newLine(dynamic_array*, signals*);
-int readChars(char*);
+int readChars(char*, FILE*);
 void shortenArray(dynamic_array*, int, char*);
 //int findTermination(char*);
 void arrayRebuild(dynamic_array*, char*, int);
 void arrayCompletion(dynamic_array*);
 
 int main() {
-    dynamic_array* init;
-    signals* firstOne;
+    dynamic_array* init = (dynamic_array*) malloc(sizeof(dynamic_array));
+    init->size = 0;
+    signals* firstOne = (signals*) malloc(sizeof(signals));
 
     int result = newLine(init, firstOne);
     return 0;
@@ -37,74 +39,53 @@ int newLine(dynamic_array* arr, signals* sig) {
     // would be best to just use the getc() function for reading chars, and check if its \n. Page 532 on textbook.
 
     FILE* inputFD = fopen("wordlist2.txt", "r");
-    int bufSize = 300; // bufferSize
+    int bufSize = 50; // bufferSize
     char* wordBuffer[15] = {0};
     int returnedByteCount;
     int index;
     while (bufSize > 0) { // we will use this to fill our arr until it is max capacity.
-    // OLD IMPLEMENTATION:
-    //     printf("%d \n", bufSize);
-    //     returnedByteCount = fread(&wordBuffer, 15, 1, inputFD); 
-    //     // ^^ We need to remove findTermination and make this a do while loop with getchar()
-    //     if (returnedByteCount <= 0) { // > 0 response is successful operation.
-    //         printf("%d", returnedByteCount);
-    //         printf("Read Failure.");
-    //         return -1;
-    //     }
-    //     else if (bufSize - returnedByteCount >= 0) {
-    //         index = findTermination(wordBuffer);
-    //         char* wordBufferReduced = (char*) malloc (index);
-    //         for (int i = 0; i < index; i++) { // creates a stripped (removes any extra spaces, \n, and null terminator) array 
-    //             wordBufferReduced[i] = wordBuffer[i];
-    //         }
-    //         arrayRebuild(arr, wordBufferReduced, index); // appends this new word to the array rebuild.
-    //         bufSize -= index;
-    //     }
-    //     else {
-    //         bufSize = 0;
-    //         break;
-    //     }
-    //     memset(wordBuffer, 0, 15); // resets wordBuffer values to all 0
-    // }
-        printf("buffer size: %d \n", bufSize);
-        dynamic_array* tempArr; // repeatedly redefine in order to reset back to empty.
+        dynamic_array* tempArr;
         char tempBuffer[15];
-        returnedByteCount = readChars(tempBuffer);
-        printf("Returned byte count: %d \n", returnedByteCount);
+        returnedByteCount = readChars(tempBuffer, inputFD);
+        printf("Buffer Size: %d, Returned byte count: %d \n", bufSize, returnedByteCount);
         if (bufSize - returnedByteCount > 0) {
             bufSize -= returnedByteCount;
+
+            // REAL VS PREDICTED SIZES ARE LIKE LEGIT WRONG IN THESE 2 FUNCTIONS. MAYBE VARIABLE TYPE SIZE RELATED
             shortenArray(tempArr, returnedByteCount, tempBuffer);
+            printf("TempArr size: %d, is supposed to be: %d\n", sizeof(tempArr), tempArr->size);
             arrayRebuild(arr, tempArr->array, returnedByteCount);
+            printf("Size of buffer atm: %d. Is supposed to be %d\n", sizeof(arr), arr->size);
         }
         else {
             printf("Filled the buffer.");
+            break;
         }
-        // handle the reduction in bufSize and also write error handling / error codes. 
     }
     fclose(inputFD); 
     arrayCompletion(arr);
     printf("Exited loop!\n");
     printf("Char count: %d \n", arr->size);
-    // now we can start to display all of the information.
-    //for (int i = 0; i < arr->size; i++) {
-        //printf("%c \n", arr->array[i]);
-    //}
-    sig->ready_signal = 1; // indicates this process has completed its job.
+    printf("Size of array: %d", sizeof(arr->array));
+
+    for (int i = 0; i < arr->size; i++) {
+        printf("%c", arr->array[i]);
+    }
+    sig->ready_signal = 1; // indicates this child has finished its job
     return 1;
 }
 
-int readChars(char* tempBuffer) { // will return the number of bytes read.
+int readChars(char* tempBuffer, FILE* fdin) { // will return the number of bytes read.
     int index = 0;
     int currentChar;
     while (index < 15) { // words have hard limit of 15 chars
-        printf("Current Index: %d \n", index);
-        currentChar = getchar();
-        if (currentChar != '\n' || currentChar != '\0') { // only terminators for a line/word
-            tempBuffer[index] = currentChar;
-            index++;
+        currentChar = getc(fdin);
+        if (currentChar == '\n' || currentChar == '\0') { // only terminators for a line/word
+            break;
         }
         else {
-            break;
+            tempBuffer[index] = currentChar;
+            index++;
         }
     }
     return index; // return number of chars (bytes) read.
@@ -118,42 +99,30 @@ void shortenArray(dynamic_array* arr_pointer, int index, char* tempBuffer) {
         arr_pointer->array[i] = tempBuffer[i];
     }
 }
-// no longer used. fault in idea.
-// int findTermination(char* wordBuffer) { // Can't see why this would fail.
-//     for (int i = 0; i < 15; i++) {
-//         if (wordBuffer[i] == '\0' || wordBuffer[i] == '\n' || wordBuffer[i] == '\t') { // only terminator cases I can think of 
-//             printf("Found problem: %c \n", wordBuffer[i]);
-//             return i; // end 1 before the terminator char if we find them.
-//         }
-//         else {
-//             printf("%c", wordBuffer[i]);
-//         }
-//     }
-//     printf("\n");
-//     return 14; // otherwise it made it to the max length.
-// }
 
 void arrayRebuild(dynamic_array* arr_pointer, char* arr2, int index) {
     // arg1 is existing array, arg2 is new array, arg3 is length of new array.
     // appends arg1 and arg2 and replaces dynamic_array struct with new one.
-    dynamic_array *newArray = arr_pointer;
-    int oldSize = arr_pointer->size;
-    int newSize = oldSize + index;
-    arr_pointer->array = (char*) calloc(newSize + 1, 1); // callocs to new size.
 
-    // These 2 loops write all of the existing and new entries to the array.
+    dynamic_array *newArray = arr_pointer;
+    arr_pointer->wordCount++;
+    int oldSize = arr_pointer->size;
+    int newSize = oldSize + index + 1;
+    arr_pointer->array = (char*) calloc(newSize, sizeof(char)); // callocs to new size.
+
     for (int i = 0; i < oldSize; i++) { // adds all of the old words
         arr_pointer->array[i] = (char) newArray->array[i];
     }
-    arr_pointer->array[arr_pointer->size] = ' '; // adds a space before the new word.
-    int shift = oldSize + 1;
-    for (int i = 0; i < index; i++) {
-        arr_pointer->array[i+shift] = (char) arr2[i]; // shift total array forward by old count, since we are appending new array.
+
+    int shift = oldSize;
+    for (int i = 0; i < index; i++) { // adds the new word. For readability
+        arr_pointer->array[i+shift] = (char) arr2[i];
     }
-    arr_pointer->size = newSize + 1; // size + 1 to handle the space we added.
+    arr_pointer->array[newSize] = ' '; // add a space at end because its in between words.
+    arr_pointer->size = newSize;
 }
 
-void arrayCompletion(dynamic_array* arr) { // Should work.
+void arrayCompletion(dynamic_array* arr) { // i don't know if i actually need this or not. depends on later implentation i suppose.
     dynamic_array* newArray = arr;
     arr->array = (char*) calloc(newArray->size + 1, 1); // increment by one to handle the null terminator
 
