e7da6eaaad78eed8657abbfd5af718f534fa5967
Tanner Swanson <tannerswanson@protonmail.com>
2024-01-06 23:08:15 -0500

    Added verification side. Provides color only in vscode terminal, not cmd

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pthread.h>
 #include <conio.h>
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 typedef struct {
-    // Utilized by newLine and validate functions.
     int size;
     int* array;
+    int wordCount;
 } dynamic_array;
 
 void readChars(dynamic_array* arr_pointer, FILE* fdin) {
@@ -26,6 +29,7 @@ void readChars(dynamic_array* arr_pointer, FILE* fdin) {
     }
     arr_pointer->array[index] = ' ';
     arr_pointer->size = index;
+    index = 0;
 }
 
 void arrayRebuild(dynamic_array* arr_og, dynamic_array* arr_new) {
@@ -41,55 +45,74 @@ void arrayRebuild(dynamic_array* arr_og, dynamic_array* arr_new) {
     arr_og->size = newSize;
 }
 
-void * newLine(void * arr_input) {
-    dynamic_array *arr = arr_input;
+int newLine(dynamic_array* arr) {
     FILE* inputFD = fopen("wordlist2.txt", "r");
     int bufSize = 50;
-    int* wordBuffer[15] = {0};
 
     while (bufSize > 0) { // we will use this to fill our arr until it is max capacity.
-        printf("Buffersize: %d\n", bufSize);
         dynamic_array tempArr = {.size = 0, .array = calloc(1, sizeof(int))};
         readChars(&tempArr, inputFD);
         if (bufSize - tempArr.size > 0) {
             bufSize -= tempArr.size;
+            arr->wordCount += 1;
             arrayRebuild(arr, &tempArr);
         }
         else {
             break;
         }
     }
-
-    fclose(inputFD); 
-
+    fclose(inputFD);
+    arr->size--; // this will account for the final character being a space due to readChars always appending.
     for (int i = 0; i < arr->size; i++) {
         printf("%c", arr->array[i]);
     }
-
-    pthread_exit(0);
+    printf("\n");
+    return 0;
 }
 
-void* validateResults(void* arr_input) {
-    dynamic_array* arr = (dynamic_array*) arr_input;
-    // need conic.h, or can just read at the end when the user hits enter. Whichever feels better
-    pthread_exit(0);
+int checkChar(dynamic_array* arr, int index, int value) {
+    int result = (int) arr->array[index] == value;
+    if (result) {
+        printf(ANSI_COLOR_GREEN "%c" ANSI_COLOR_RESET, value);
+    }
+    else {
+        printf(ANSI_COLOR_RED "%c" ANSI_COLOR_RESET, value);
+    }
+    return result;
+}
+
+int validateRespones(dynamic_array* arr) {
+    int* resultArray = calloc(arr->size, sizeof(int));
+    int currentIndex = 0;
+    while (currentIndex < arr->size) {
+        int character = getch();
+        if (character == 64) { // exit
+            return -1;
+        }
+        else if (character == 8) { // backspace
+            if (currentIndex > 0) { // if index is 0, ignores
+                printf("\b");
+                currentIndex--;
+            }
+        }
+        else { // do 
+            resultArray[currentIndex] = checkChar(arr, currentIndex, character);
+            currentIndex++;
+        }
+    }
+    printf("\n");
+    printf("Finished!");
+    for (int i = 0; i < arr->size; i++) { 
+        printf("%d", resultArray[i]);
+    }
+    return 0;
 }
 
 int main() {
     // Child a (reads from wordlist2.txt, sets aside 300ish chars, prints out)
     // Child b validates user input against the array from child a. (in from terminal, out to terminal, maybe to a third results thread)
     dynamic_array arr = { .size = 0, .array = calloc(1, sizeof(int))};
-
-    pthread_t childA;
-    pthread_t childB;
-    pthread_create(&childA, NULL, newLine, &arr); // the code worked prior to threads implementation. now only works in gdb.
-    if (!pthread_join(childA, NULL)) { 
-        // success - now send out child b.
-        printf("yooo\n");
-        pthread_create(&childB, NULL, validateResults, &arr);
-    }
-    else {
-        printf("nooooo\n");
-    }
+    newLine(&arr);
+    validateRespones(&arr);
     return 0;
 }
