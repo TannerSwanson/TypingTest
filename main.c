99a06867cdfb0d7b54d7e0654645ca394d2b8c6a
Tanner Swanson <tannerswanson@protonmail.com>
2024-01-16 21:27:05 -0500

    Adding timing.

--- main.c
+++ main.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <conio.h>
+#include <time.h>
 
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
-
+#define BUF_SIZE            100
 typedef struct {
     int size;
     int* array;
@@ -43,13 +44,27 @@ void arrayRebuild(dynamic_array* arr_og, dynamic_array* arr_new) {
     }
     arr_og->array = tempArray;
     arr_og->size = newSize;
+    arr_og->wordCount++;
 }
 
-int newLine(dynamic_array* arr) {
-    FILE* inputFD = fopen("wordlist2.txt", "r");
-    int bufSize = 50;
+int printLine(dynamic_array* arr) {
+    printf("\n");
+    for (int i = 0; i < arr->size; i++) {
+        printf("%c", arr->array[i]);
+    }
+    printf("\n");
+    return 0;
+}
 
+int addNewLine(dynamic_array* arr) {
+    FILE* inputFD = fopen("wordlist2.txt", "r");
+    if (inputFD == NULL) {
+        printf("fopen() failed.\n");
+        return -1;
+    }
+    int bufSize = BUF_SIZE;
     while (bufSize > 0) { // we will use this to fill our arr until it is max capacity.
+        printf("Bufsize: %d\n", bufSize);
         dynamic_array tempArr = {.size = 0, .array = calloc(1, sizeof(int))};
         readChars(&tempArr, inputFD);
         if (bufSize - tempArr.size > 0) {
@@ -63,10 +78,7 @@ int newLine(dynamic_array* arr) {
     }
     fclose(inputFD);
     arr->size--; // this will account for the final character being a space due to readChars always appending.
-    for (int i = 0; i < arr->size; i++) {
-        printf("%c", arr->array[i]);
-    }
-    printf("\n");
+    printLine(arr); //print to terminal for user.
     return 0;
 }
 
@@ -84,13 +96,23 @@ int checkChar(dynamic_array* arr, int index, int value) {
 int validateRespones(dynamic_array* arr) {
     int* resultArray = calloc(arr->size, sizeof(int));
     int currentIndex = 0;
+    clock_t start, total;
+    float timeTyping;
+    int hasTimerStarted = 0;
+
     while (currentIndex < arr->size) {
         int character = getch();
-        if (character == 64) { // exit
+        if (!hasTimerStarted) {
+            hasTimerStarted = 1;
+            start = clock();
+        }
+        if (character == 64) { // exit (@)
             return -1;
         }
         else if (character == 8) { // backspace
             if (currentIndex > 0) { // if index is 0, ignores
+                printf("\b"); // goes back, prints space to wipe, goes back again
+                printf(" ");
                 printf("\b");
                 currentIndex--;
             }
@@ -100,19 +122,29 @@ int validateRespones(dynamic_array* arr) {
             currentIndex++;
         }
     }
-    printf("\n");
-    printf("Finished!");
-    for (int i = 0; i < arr->size; i++) { 
-        printf("%d", resultArray[i]);
+    total = clock() - start;
+
+    int sum = 0;
+     for (int i = 0; i < arr->size; i++) { 
+        //printf("%d", resultArray[i]);
+        sum += resultArray[i];
     }
+    timeTyping = (float) total / CLOCKS_PER_SEC;
+    printf("\nCorrect characters / Incorrect Characters / Percentage:");
+    printf(" %d / %d / %lf%%\n", sum, arr->size-sum, (double) sum/arr->size * 100);
+    printf("Seconds elapsed: %f\n", timeTyping);
+    printf("Seconds per character: %lf\n", timeTyping / (float) arr->size);
+
     return 0;
 }
 
 int main() {
-    // Child a (reads from wordlist2.txt, sets aside 300ish chars, prints out)
-    // Child b validates user input against the array from child a. (in from terminal, out to terminal, maybe to a third results thread)
+    //  now we need to do:
+    //          More error checking / handling
+    //          More results (total words correct / total)
+    //          Get normal compilation working again :(
     dynamic_array arr = { .size = 0, .array = calloc(1, sizeof(int))};
-    newLine(&arr);
+    addNewLine(&arr);
     validateRespones(&arr);
     return 0;
 }
